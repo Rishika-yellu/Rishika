a909fa33f5faa407bbf3a46d8a83092edfdf4468
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 10:49:47 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,18 @@
+
+package mypro;
+
+
+public class BitwiseOperators {
+    public static void main(String args[])
+    {
+        int a=25;
+        int b=15;
+        int c=a&b;
+        int d=a|b;
+        System.out.println(c);
+        System.out.println(d);
+        
+        
+    }
+    
+}
