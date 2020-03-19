0ab4b35dcb873ea181b373d4aeb41ea6701f9b15
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 09:40:55 +0530

    Add files via upload

---
+++ Add2.java
@@ -0,0 +1,10 @@
+class Add2
+{
+      public static void main(String a[])
+      {
+          int num1=6;
+          int num2=7;
+          int sum=num1+num2;
+          System.out.print(sum);
+       }
+}
