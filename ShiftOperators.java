e7a3cd836eb2de21df5fbe1638af64ab350985f6
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 10:54:32 +0530

    Add files via upload

---
+++ ShiftOperators.java
@@ -0,0 +1,15 @@
+
+package mypro;
+
+
+public class ShiftOperators {
+    public static void main(String args[])
+    {
+        int a=7;
+        int b=a>>2;
+        int c=a<<2
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
