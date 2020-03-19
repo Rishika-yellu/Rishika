d998a78a9f51288720373f79153868f6a3ee9cb1
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 11:17:44 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,26 @@
+
+package mypro;
+
+public class SwitchCase {
+    public static void main(String args[])
+    {
+        int i=6;
+        switch(i)
+        {
+            case 0:System.out.println("zero");
+                   break;
+            case 1:System.out.println("one");
+                   break;
+            case 2:System.out.println("two");
+                   break;
+            case 4:System.out.println("four");
+                   break;
+            case 5:System.out.println("five");
+                   break;
+            default:System.out.println("somenumber");
+                    break;
+                   
+        }
+    }
+    
+}
