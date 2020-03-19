fa3b3af4880a111487e76a0af6e5b55d061018a6
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 11:02:47 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+
+package mypro;
+
+
+public class IfElse 
+    {
+        public static void main(String args[])
+        {
+           int n1=9;
+           if(n1%2==0)
+           {
+               System.out.println("even");
+           }
+           else{
+               System.out.println("odd");
+           }
+        }
+    }
+    
