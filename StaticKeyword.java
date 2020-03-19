da603e1f6205c2f62b9e8a4b67756668061ccb78
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 15:08:56 +0530

    Add files via upload

---
+++ StaticKeyword.java
@@ -0,0 +1,20 @@
+
+package mypro;
+
+
+public class StaticKeyword {
+    public static void main(String args[])
+    {
+        Pqr.show();
+        
+    }
+    
+}
+ class Pqr
+{
+     static int i=5;
+    public static void show()
+    {
+        System.out.println(i);
+    }
+}
