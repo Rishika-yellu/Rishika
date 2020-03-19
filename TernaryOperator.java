9370d375c155f766ea3540868cadf9d3795d7e44
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 11:09:17 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,13 @@
+
+package mypro;
+
+
+public class TernaryOperator {
+    public static void main(String args[])
+    {
+        int i=6;
+        int j=i==6?1:0;
+        System.out.println(j);
+    }
+    
+}
