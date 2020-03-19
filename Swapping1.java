d99fc586a34681e8ba5be93836d1c700d0410de4
Rishika-yellu <36666802+Rishika-yellu@users.noreply.github.com>
2020-03-19 12:44:55 +0530

    Add files via upload

---
+++ Swapping1.java
@@ -0,0 +1,18 @@
+
+package mypro;
+
+
+public class Swapping1 {
+    public static void main (String args[])
+    {
+        int a=5;
+        int b=6;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        
+    }
+    
+}
